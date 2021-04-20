a0ae779cd37e53e49950a94c31e7e02bba0c1d25
Natalia Kirdis <kirdisnatalia@gmail.com>
2021-04-20 11:42:01 +0300

    [addressbook]: Added contact creation test

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,43 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+  private final String name;
+  private final String middlename;
+  private final String lastname;
+  private final String address;
+  private final String phone;
+  private final String email;
+
+  public ContactData(String name, String middlename, String lastname, String address, String phone, String email) {
+    this.name = name;
+    this.middlename = middlename;
+    this.lastname = lastname;
+    this.address = address;
+    this.phone = phone;
+    this.email = email;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getMiddlename() {
+    return middlename;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getPhone() {
+    return phone;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+}
