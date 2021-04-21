ae1d37f1e0a230747a8b277987e2082573c83615
Natalia Kirdis <kirdisnatalia@gmail.com>
2021-04-21 23:45:56 +0300

    [addressbook]: added preconditions for group tests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -8,11 +8,14 @@ public class GroupModificationTests extends TestBase {
   @Test
   public void testGroupModification() {
     app.getNavigationHelper().gotoGroupPage();
+    if (! app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("test1",
+              "test1 header","test1 footer"));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForm(new GroupData("test1 edited",
-                                                     "test1 header edited",
-                                                     "test1 footer edited"));
+            "test1 header edited","test1 footer edited"));
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
   }
