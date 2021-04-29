fa3380d305c4375add0730b751a64da6482d119b
Natalia Kirdis <kirdisnatalia@gmail.com>
2021-04-29 19:44:09 +0300

    [addressbook]: get groups test data from json

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
@@ -2,6 +2,8 @@ package ru.stqa.pft.addressbook.generators;
 
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.thoughtworks.xstream.XStream;
 import ru.stqa.pft.addressbook.models.GroupData;
 
@@ -38,9 +40,19 @@ public class GroupDataGenerator {
       saveAsCsv(group, new File(file));
     } else if (format.equals("xml")){
       saveAsXml(group, new File(file));
+    } else if (format.equals("json")) {
+      saveAsJson(group, new File(file));;
     } else {
       System.out.println("Unrecognized format " + format);
     }
+  }
+
+  private void saveAsJson(List<GroupData> groups, File file) throws IOException {
+    Gson gson = new GsonBuilder().setPrettyPrinting().excludeFieldsWithoutExposeAnnotation().create();
+    String json = gson.toJson(groups);
+    Writer writer = new FileWriter(file);
+    writer.write(json);
+    writer.close();
   }
 
   private void saveAsXml(List<GroupData> groups, File file) throws IOException {
